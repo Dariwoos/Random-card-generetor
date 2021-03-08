fda6ac95364689e0743ce88fa6a78c3c496719d8
Mohamad Alzaim <tst.info20@gmail.com>
2021-03-08 17:44:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-let numberarray = ["1","2","3","4","5","6","7","8","9","10","J","k"];
-let arrayicon = ["\u2665","\u2666","\u2663","\u2660"];
+let numberArray = ["1","2","3","4","5","6","7","8","9","10","J","k"];
+let arrayIcon = ["\u2665","\u2666","\u2663","\u2660"];
+
+function random  (myArray){
+    let randomCard = Math.floor(Math.random() * (myArray.length));
+    return random;
+
+}
+let numbersArray = numberArray[random(numberArray)];
+let arrayIcons = arrayIcon[random(arrayIcon)];
 
 
-var arraynumberIndex = Math.floor(Math.random() * numberarray.length);
-var arrayiconIndex = Math.floor(Math.random() * arrayicon.length);
-
-if(arrayicon=="\u2665" || arrayicon=="\u2666" ){
-    console.log(true);
+if(arrayIcon=="\u2665" || arrayIcon=="\u2666" ){
     document.querySelector("#leftIcon").style.color="red";
     document.querySelector("#righIcon").style.color="red";
     document.querySelector("#num").style.color="red";
-
 }else{
     document.querySelector("#leftIcon").style.color="black";
     document.querySelector("#righIcon").style.color="black";
@@ -18,6 +21,6 @@ if(arrayicon=="\u2665" || arrayicon=="\u2666" ){
 
 }
 
-document.getElementById("num").innerHTML = numberarray[arraynumberIndex];
-document.getElementById("leftIcon",).innerHTML = arrayicon[arrayiconIndex];
-document.getElementById("righIcon",).innerHTML = arrayicon[arrayiconIndex];
+document.getElementById("num").innerHTML = numberArray;
+document.getElementById("leftIcon",).innerHTML = arrayIcons;
+document.getElementById("righIcon",).innerHTML = arrayIcons;
