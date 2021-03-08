eb4ba6ba10a31c2e77367799cd46efb3f3008df2
Mohamad Alzaim <tst.info20@gmail.com>
2021-03-08 17:32:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 let numberarray = ["1","2","3","4","5","6","7","8","9","10","J","k"];
-let arrayicon = ["♥","♣","♦","♠"];
+let arrayicon = ["\u2665","\u2666","\u2663","\u2660"];
 
 
 var arraynumberIndex = Math.floor(Math.random() * numberarray.length);
 var arrayiconIndex = Math.floor(Math.random() * arrayicon.length);
 
-let stynumbers = document.getElementById("num");
-stynumbers.style.fontSize= "150px";
-let syuicon= document.getElementById("righIcon");
+if(arrayicon=="\u2665" || arrayicon=="\u2666" ){
+    console.log(true);
+    document.querySelector("#leftIcon").style.color="red";
+    document.querySelector("#righIcon").style.color="red";
+    document.querySelector("#num").style.color="red";
 
+}else{
+    document.querySelector("#leftIcon").style.color="black";
+    document.querySelector("#righIcon").style.color="black";
+    document.querySelector("#num").style.color="black";
 
-
+}
 
 document.getElementById("num").innerHTML = numberarray[arraynumberIndex];
 document.getElementById("leftIcon",).innerHTML = arrayicon[arrayiconIndex];
